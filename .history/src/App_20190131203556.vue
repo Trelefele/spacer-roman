c64a16ff0376c158d936770fdaafbf5fe2d8f2d1
unknown <karol1854@gmail.com>
2019-01-31 20:47:11 +0100

    Blank template

--- .history/src/App_20190131203556.vue
+++ .history/src/App_20190131203556.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app">
     <div class="app">
-      <router-link to="/home">spacer</router-link>
-      <router-link to="/about">about</router-link>
+      <router-link to="/home">spacer<router-link/>
+      <router-link to="/about">about<router-link/>
     </div>
   </div>
 </template>
